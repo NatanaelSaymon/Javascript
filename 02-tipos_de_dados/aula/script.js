530b203b3a7bbb4f8ceab32be6028f871382b1ab
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-26 16:05:23 -0300

    Done: tipos de dados

---
+++ 02-tipos_de_dados/aula/script.js
@@ -0,0 +1,16 @@
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+//Vericador de TIPO DE DADO: TYPEOF
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof possuiFaculdade)
+console.log(typeof time)
+console.log(typeof comida)
+console.log(typeof simbolo)
+console.log(typeof novoObjeto)
---
+++ 02-tipos_de_dados/exercicio/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var dog = "Cacau"
+
+// Declare uma variável contendo um número dentro de uma string
+var numeroString = "28"
+
+// Declare uma variável com a sua idade
+var idade = 28
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = "Natanael"
+var sorbeNome = "Souza"
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof dog)
+console.log(typeof numeroString)
+console.log(typeof idade)
+console.log(typeof nome)
+console.log(typeof sorbeNome)
+console.log(typeof frase)
