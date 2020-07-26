928255738935d3179ba890c9eb4a76b8b381f813
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-26 15:21:18 -0300

    Done: aula 01 - variavel

---
+++ 01-variaveis/Aula/scritp.js
@@ -0,0 +1,4 @@
+var nome = "Natan"
+var idade = 28
+
+console.log(`O usuario ${nome}, tem ${idade} anos`)
---
+++ 01-variaveis/Exercicio/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'Natanael Saymon'
+
+// Declarar uma variável com a sua idade
+var idade = 28
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "feijoda"
+
+console.log(`Nome: ${nome}, idade: ${idade}, comida favorita: ${comidaFavorita}`)
+
+// Declarar 5 variáveis diferentes sem valores
+
+var cor,
+  sexo,
+  heroiFavorito,
+  filmeFavorito,
+  amorDaMinhaVida
