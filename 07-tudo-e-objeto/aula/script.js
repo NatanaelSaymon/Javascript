670b0c50a69c7155802392ae939b9f11791e77d0
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-28 10:47:22 -0300

    Done: Tudo é Ojetos - aula

---
+++ 07-tudo-e-objeto/aula/script.js
@@ -0,0 +1,11 @@
+var nome = "Natanael"
+
+console.log(nome.toLocaleLowerCase())
+console.log(nome.length)
+
+var altura1 = 1.75
+var altura2 = 1.80
+var alturaString = altura2.toString() //Metodo toString(): Transforma por um breve momento o numero em uma string
+
+console.log(altura1.toFixed())
+console.log(typeof alturaString)
