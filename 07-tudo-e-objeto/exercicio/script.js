746b9012458596771b743f508854069d6c0d114f
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-28 17:27:57 -0300

    Done: propriedades e metodos - exercicio

---
+++ 07-tudo-e-objeto/exercicio/script.js
@@ -0,0 +1,25 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = "Saymon"
+console.log(nome.length)
+console.log(nome.toLocaleLowerCase())
+console.log(nome.toLocaleUpperCase())
+console.log(nome.slice(0, 2))
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// const btn = document.querySelector('.btn1')
+// btn.addEventListener('click', function () {
+//   console.log('clicou', document.characterSet)
+// })
+
+//document.addEventListener()
+document.bgColor = "gray"
+//document.contains()
+//document.createAttribute()
+
+
+
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
