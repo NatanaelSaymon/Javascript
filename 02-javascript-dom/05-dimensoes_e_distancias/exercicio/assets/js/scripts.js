9dea7c201c8a0603ece91fe7a37d4331cbe8947f
Natanael Saymon <nsaymonsouza@gmail.com>
2020-08-02 16:33:48 -0300

    Done: dimensoes e distancias - exercicio

--- 02-javascript-dom/05-dimensoes_e_distancias/exercicio/assets/js/scripts.js
+++ 02-javascript-dom/05-dimensoes_e_distancias/exercicio/assets/js/scripts.js
@@ -1,62 +1,52 @@
-// //CLASSLIST
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
 
-// const menu = document.querySelector(".menu");
-// console.log(menu.classList);
+const imgTop = document.querySelector('img')
+console.log(imgTop.offsetTop)
 
-// //CONTAINS = true ou false
-// if (menu.classList.contains("inativo")) {
-//   console.log("Tem a class inativo no menu");
-// } else {
-//   console.log("Não tem class inativo no menu");
-// }
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const allImg = document.querySelectorAll('img')
 
-// //ATTRIBUTE: retorna um array-like com os atributos do elemento
-// const animais = document.querySelector(".animais");
-// console.log(animais.attributes); //retorna todos os atributos
-// console.log(animais.attributes[1]); //retorna o 2 atributos
+  let totalWidth = 0
+  allImg.forEach((img) => {
+    totalWidth += img.offsetWidth
+  })
 
-// //GETATRIBUTE / SETATRIBUTE
-// const animaisImagem = document.querySelectorAll("img");
+  console.log("A soma da largura de todas as imagens é: ", totalWidth)
+}
 
-// // animaisImagem.forEach((item) => {
-// //   console.log(item.getAttribute("alt")); //retorna todos os atributos alts das imagens.
-// // });
-
-// animaisImagem.forEach((item) => {
-//   if (item.hasAttribute("alt") == false) {
-//     //Verifica se cada imagem contem o atributo alt, caso n tenha, vai ser adicionado o alt com um texto
-//     item.setAttribute("alt", "Texto alternativo");
-//   }
-//   console.log(item.getAttribute("alt"));
-// });
+window.onload = function () {
+  somaImagens()
+}
 
 
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
 
+const links = document.querySelectorAll('a')
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth
+  const linkHeight = link.offsetHeight
 
-//EXERCICIO
-
-// Adicione a classe ativo a todos os itens do menu
-const menuItems = document.querySelectorAll('.menu ul li a')
-menuItems.forEach((item) => {
-  item.classList.add('ativo')
+  if (linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, 'Possi boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibildiade')
+  }
 })
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-menuItems.forEach((item) => {
-  item.classList.remove('ativo')
-})
-menuItems[0].classList.add('ativo')
 
 
-// Verifique se as imagens possuem o atributo alt
-const allImage = document.querySelectorAll('img')
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
 
-allImage.forEach((imgItem) => {
-  const possuiAtributo = imgItem.hasAttribute('alt')
-  console.log(possuiAtributo)
-})
+const widthBrowser = window.innerWidth
+const menu = document.querySelector('.menu')
 
-// Modifique o href do link externo no menu
-const linkExterno = document.querySelector('a[href^="http"]')
-
-linkExterno.setAttribute('href', '#')
+if (widthBrowser < 720) {
+  menu.classList.add('menu-mobile')
+} else {
+  console.log('Largura da tela: ', widthBrowser)
+}
