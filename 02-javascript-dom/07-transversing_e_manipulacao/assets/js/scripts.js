1e37d1ac1d09fb078e23da4906446c7e065e03f4
Natanael Saymon <nsaymonsouza@gmail.com>
2020-08-05 10:31:40 -0300

    Done: Aula - Transversing e Manipulação

--- 02-javascript-dom/07-transversing_e_manipulacao/assets/js/scripts.js
+++ 02-javascript-dom/07-transversing_e_manipulacao/assets/js/scripts.js
@@ -1,12 +1,12 @@
 //TRANSVERSING E MANIPULAÇÃO
 
-const h1 = document.querySelector('h1')
+const h1 = document.querySelector("h1");
 
 // h1.innerText = "Novo titulo"
 // console.log(h1.outerHTML)
 // console.log(h1.innerHTML)
 
-const lista = document.querySelector('.animais-lista')
+const lista = document.querySelector(".animais-lista");
 
 // console.log(lista.parentElement) // pai
 // console.log(lista.parentElement.parentElement) // pai do pai
@@ -17,11 +17,40 @@ const lista = document.querySelector('.animais-lista')
 // console.log(lista.children[0]) //primeiro filho
 // console.log(lista.children[--lista.children.length]) //retorna o ultimo filho
 
-const ul = lista.querySelectorAll('li') // todas as LI's
-const last_ul = lista.querySelector('li:last-child')
+const ul = lista.querySelectorAll("li"); // todas as LI's
+const last_ul = lista.querySelector("li:last-child");
 
-ul.forEach((item) => {
-  console.log(item)
-})
+// ul.forEach((item) => {
+//   console.log(item)
+// })
 
-console.log('Ultimo filho: ', last_ul)
+// console.log('Ultimo filho: ', last_ul)
+
+const contato = document.querySelector(".contato"); //pegando a todos os elementos que estão dentro de .contato
+
+const titulo = document.querySelector(".title"); //Pegando o primeiro titulo da pagina
+
+const tituloContato = contato.querySelector(".title"); //Estamos pegando o primeiro titulo da class "CONTATO"
+
+const mapa = document.querySelector(".mapa");
+
+const faq = document.querySelector(".faq");
+
+// contato.appendChild(lista); //Move o elemento "lista" para o final de "contato"
+
+// contato.insertBefore(lista, tituloContato); //Movendo a lista para antes do titulo de contato. Lembrando o segundo elemento passado dentro do metodo insertBefore() este elemento tem que está dentro do elemento Pai(contato)
+
+// contato.removeChild(tituloContato); //Removendo o titulo de contato
+
+// contato.replaceChild(lista, tituloContato); //Substitui o titulo de contato pela lista
+
+// const newh1 = document.createElement("h1"); //Criando um novo elemento com o metodo createElement()
+
+// newh1.innerText = "Novo titulo";
+// newh1.classList.add("title");
+
+// mapa.appendChild(newh1); //Adicionando o novo elemento na pagina
+
+const cloneTitulo = titulo.cloneNode(true);
+
+faq.appendChild(cloneTitulo);
