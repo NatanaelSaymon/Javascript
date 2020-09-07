1d701296b7b4e695913140099ce72ca80cf419cf
Natanael Saymon <nsaymonsouza@gmail.com>
2020-09-07 16:12:36 -0300

    Done: inserindo arquivo do menu drop down

--- 05-javascript-efeitos-no-DOM/04-menu-drop-down/assets/js/scripts.js
+++ 05-javascript-efeitos-no-DOM/04-menu-drop-down/assets/js/scripts.js
@@ -2,11 +2,13 @@ import initScrollAnimation from './modules/scroll-animation.js'
 import initAccordionList from './modules/accordion.js'
 import initTabNav from './modules/navTab.js'
 import initModal from './modules/modal.js'
+import initDropDownMenu from './modules/drop-down-menu.js'
 
 initScrollAnimation()
 initAccordionList()
 initTabNav()
 initModal()
+initDropDownMenu()
 
 
 
