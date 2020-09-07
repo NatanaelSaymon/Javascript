0e96eac1191d75668ff328023c405dab141e5785
Natanael Saymon <nsaymonsouza@gmail.com>
2020-09-07 17:10:31 -0300

    Done: script do menu drop down

--- 05-javascript-efeitos-no-DOM/04-menu-drop-down/assets/js/modules/drop-down-menu.js
+++ 05-javascript-efeitos-no-DOM/04-menu-drop-down/assets/js/modules/drop-down-menu.js
@@ -1,3 +1,20 @@
 export default function initDropDownMenu(){
+  const dropDownMenus = document.querySelectorAll('[data-dropdown]')
 
+  function handleClick(event){
+    event.preventDefault();
+    this.classList.toggle('active')
+  }
+
+  dropDownMenus.forEach((menu) => {
+    menu.addEventListener('click', handleClick) //evento de click
+    menu.addEventListener('touchstart', handleClick) //evento de toque mobile
+
+    // Caso queira fazer de uma forma mais otimizada
+    // const eventos = ['click', 'touchstart'] //array que contem os eventos
+
+    // eventos.forEach((evento) =>{
+    //   menu.addEventListener(evento, handleClick)
+    // })
+  })
 }
