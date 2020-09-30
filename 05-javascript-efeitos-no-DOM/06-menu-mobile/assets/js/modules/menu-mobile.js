0e39819c3b459920f569cde2e3d8b10c27b969df
Natanael <nsaymonsouza@gmail.com>
2020-09-30 12:15:02 -0300

    Algumas melhorias no script

--- 05-javascript-efeitos-no-DOM/06-menu-mobile/assets/js/modules/menu-mobile.js
+++ 05-javascript-efeitos-no-DOM/06-menu-mobile/assets/js/modules/menu-mobile.js
@@ -3,16 +3,22 @@ import outsideClick from './outsideclick.js'
 export default function initMenuMobile(){
     const menuButton = document.querySelector('[data-menu="button"]')
     const menuList = document.querySelector('[data-menu="list"]')
+    const eventos = ['click', 'touchstart']
 
-    function openMenu(){
-        menuButton.classList.add('active')
-        menuList.classList.add('active')
-
-        outsideClick(menuList, ["click", "touchstart"], () => {
-            menuButton.classList.remove('active')
-            menuList.classList.remove('active')
+    if(menuButton){ //o codigo abaixo soh ira execuar caso tenha o menuButton na pagina.
+        function openMenu(){
+            menuButton.classList.add('active')
+            menuList.classList.add('active')
+    
+            outsideClick(menuList, eventos, () => {
+                menuButton.classList.remove('active')
+                menuList.classList.remove('active')
+            })
+        }
+    
+        eventos.forEach((evento) => {
+            menuButton.addEventListener(evento, openMenu)
         })
     }
-
-    menuButton.addEventListener("click", openMenu)
+    
 }
