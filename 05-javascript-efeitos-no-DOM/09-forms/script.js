7211e83e9e31a81a3978f378d4b3517328bd5f75
Natanael <nsaymonsouza@gmail.com>
2020-10-02 16:53:33 -0300

    form

--- 05-javascript-efeitos-no-DOM/09-forms/script.js
+++ 05-javascript-efeitos-no-DOM/09-forms/script.js
@@ -6,13 +6,14 @@ function validacao(event){
 
     if(!target.checkValidity()){
         target.classList.add('invalido')
-        target.nextElementSibling.innerText = target.validationMessage 
-        //inseri uma msg dentro do proximo elemento, neste caso "span"
-    }
-    else{
+        target.nextElementSibling.innerText = target.validationMessage //inseri uma msg dentro do proximo elemento, neste caso "span"
+    }else{
         target.classList.remove('invalido')
         target.nextElementSibling.innerText = ""
     }
+
+    //document.body.style.backgroundColor = event.target.value
+    //Para fazer o codigo acima funcionar, basta deixar o if e else comentado.
 }
 
 formulario.addEventListener('change', validacao)
